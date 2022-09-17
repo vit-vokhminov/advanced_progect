f88f6bec286415d9454d81e37afdf7079223764e
vit-vokhminov <vit-vokhminov@yandex.ru>
2022-09-17 23:04:14 +0700

    4 Подключаем React и настраиваем css в webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,5 +7,17 @@ export function buildLoaders(): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     };
 
-    return [typescriptLoader];
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from js strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+        ],
+    };
+
+    return [typescriptLoader, cssLoader];
 }
