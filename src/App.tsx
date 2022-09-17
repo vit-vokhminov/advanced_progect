b52c142ddf4d6b8c4db6e1b2df389306c6a698c8
vit-vokhminov <vit-vokhminov@yandex.ru>
2022-09-17 23:08:54 +0700

    8 classNames создаем git репозиторий

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React, { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
+    const bool = true;
+
     return (
-        <div className='app'>
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
