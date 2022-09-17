008c9ecbea2e39cc4ee65214b96c1b7d49d60cfb
vit-vokhminov <vit-vokhminov@yandex.ru>
2022-09-17 23:05:41 +0700

    5 Настраиваем css modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,23 +1,34 @@
-import webpack from 'webpack';
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    };
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-            // Creates `style` nodes from js strings
-            'style-loader',
-            // Translates CSS into CommonJS
-            'css-loader',
-            // Compiles Sass to CSS
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
+                    },
+                }
+            },
             'sass-loader',
-        ],
-    };
+        ]
+    }
 
-    return [typescriptLoader, cssLoader];
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    return [
+        typescriptLoader,
+        cssLoader,
+    ]
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,15 +1,17 @@
-import path from 'path';
 import webpack from 'webpack';
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
+import path from "path";
 
-import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildEnv, BuildPaths } from './config/build/types/config';
 
 export default (env: BuildEnv) => {
+
+
     const paths: BuildPaths = {
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
         build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
-    };
+    }
 
     const mode = env.mode || 'development';
     const isDev = mode === 'development';
@@ -20,7 +22,7 @@ export default (env: BuildEnv) => {
         paths,
         isDev,
         port: PORT,
-    });
+    })
 
     return config;
 };
