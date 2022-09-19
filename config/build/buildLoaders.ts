45c71467c4f02398a4b6e08558313bb237f12339
vit-vokhminov <vit-vokhminov@yandex.ru>
2022-09-19 16:00:26 +0700

    13 Svg loader. File loader. Button UI kit

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -30,8 +30,19 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     }
 
-    return [
-        typescriptLoader,
-        cssLoader,
-    ]
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    };
+    
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    };
+
+    return [fileLoader, svgLoader, typescriptLoader, cssLoader];
 }
