bf280c24f4fe3e941c2b7dd49a7f2ce6a61839e6
vit-vokhminov <vit-vokhminov@yandex.ru>
2022-09-19 17:53:51 +0700

    15 i18n Интернационализация. Define plugin. Плагин для переводов

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,7 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Sidebar.module.scss';
-import React, { useState } from 'react';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
+import { useState } from 'react';
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
+import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
 
 interface SidebarProps {
     className?: string;
@@ -23,6 +24,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
             <button onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
+                <LangSwitcher className={cls.lang} />
             </div>
         </div>
     );
