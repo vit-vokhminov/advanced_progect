574eb04f17e84302f7cb90b58470c7a4d6efd667
vit-vokhminov <vit-vokhminov@yandex.ru>
2022-09-30 21:08:16 +0700

    25 Настраиваем Storybook. Декораторы. Стори кейсы на компоненты

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,27 +1,9 @@
-import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import {BuildOptions} from "./types/config";
+import webpack from 'webpack';
+import { buildCssLoader } from './loaders/buildCssLoader';
+import { BuildOptions } from './types/config';
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
-
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-                loader: "css-loader",
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:5]'
-                            : '[hash:base64:8]'
-                    },
-                }
-            },
-            "sass-loader",
-        ],
-    }
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    const cssLoader = buildCssLoader(isDev);
 
     const babelLoader = {
         test: /\.(js|jsx|tsx)$/,
@@ -29,30 +11,30 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         use: {
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env'],
-            },
-        },
+                presets: ['@babel/preset-env']
+            }
+        }
     };
 
     // Если не используем тайпскрипт - нужен babel-loader
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+        exclude: /node_modules/
+    };
 
     const svgLoader = {
         test: /\.svg$/,
-        use: ['@svgr/webpack'],
+        use: ['@svgr/webpack']
     };
-    
+
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff2|woff)$/i,
         use: [
             {
-                loader: 'file-loader',
-            },
-        ],
+                loader: 'file-loader'
+            }
+        ]
     };
 
     return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
