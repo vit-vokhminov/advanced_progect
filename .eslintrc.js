2b485eef6da00396d805ba6e48581c52e4dd1226
vit-vokhminov <vit-vokhminov@yandex.ru>
2022-11-22 21:30:18 +0700

    66 Семантика

--- .eslintrc.js
+++ .eslintrc.js
@@ -93,7 +93,7 @@ module.exports = {
             'error',
             {
                 markupOnly: true,
-                ignoreAttribute: ['data-testid', 'to']
+                ignoreAttribute: ['role', 'data-testid', 'to', 'target', 'justify', 'align', 'direction', 'gap'],
             }
         ],
         'no-sequences': 'off',
