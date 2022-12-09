1ff9d5d1f20c4d68e973bc5602ca78da8360bb2b
vit-vokhminov <vit-vokhminov@yandex.ru>
2022-12-09 20:26:12 +0700

    79 Popover. NotificationList. RTK query. Polling

--- .eslintrc.js
+++ .eslintrc.js
@@ -93,7 +93,7 @@ module.exports = {
             'error',
             {
                 markupOnly: true,
-                ignoreAttribute: ['as', 'role', 'data-testid', 'to', 'target', 'justify', 'align', 'direction', 'gap',],
+                ignoreAttribute: ['as', 'role', 'data-testid', 'to', 'target', 'justify', 'align', 'border', 'direction', 'gap',],
             }
         ],
         'no-sequences': 'off',
